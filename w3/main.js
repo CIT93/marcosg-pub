02b9e04fac30bfd094119bf71af2e54c7bb763e0
Marcos Gutierrez <victordavidmarcos123@gmail.com>
2025-01-28 10:35:33 -0800

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,19 +1,27 @@
-let carbonFootprintPoints = 0;
-const numberInHousehold = 3;
-
-if (numberInHousehold === 1) {
+function DHHP(numberInHousehold) {
+  console.log("inside the function");
+  if (numberInHousehold === 1) {
     carbonFootprintPoints = carbonFootprintPoints + 14;
-} else if(numberInHousehold === 2) {
+  } else if (numberInHousehold === 2) {
     carbonFootprintPoints = carbonFootprintPoints + 12;
-} else if(numberInHousehold === 3) {
+  } else if (numberInHousehold === 3) {
     carbonFootprintPoints = carbonFootprintPoints + 10;
-} else if(numberInHousehold === 4) {
+  } else if (numberInHousehold === 4) {
     carbonFootprintPoints = carbonFootprintPoints + 8;
-} else if(numberInHousehold === 5) {
+  } else if (numberInHousehold === 5) {
     carbonFootprintPoints = carbonFootprintPoints + 6;
-} else if(numberInHousehold === 6) {
+  } else if (numberInHousehold === 6) {
     carbonFootprintPoints = carbonFootprintPoints + 4;
-} else if(numberInHousehold > 6) {
+  } else if (numberInHousehold > 6) {
     carbonFootprintPoints = carbonFootprintPoints + 2;
+  }
+  console.log(`The Carbon Foot Print for a Household of ${numberInHousehold} is ${carbonFootprintPoints}.`);
 }
-console.log(`The Carbon Foot Print for a Household of ${numberInHousehold} is ${carbonFootprintPoints}.`);
+
+let carbonFootprintPoints = 0;
+// const numberInHousehold = 3;
+
+// global scope
+
+DHHP(3)
+DHHP(4)
